7984742e8bb7282f10981c07d24100ac7689ca19
AR-Shahin <mdshahinmije96@gmail.com>
2021-11-26 21:56:31 +0600

    js first

---
+++ JS/main.js
@@ -0,0 +1,105 @@
+// console.log('Hello Shahin')
+// alert("Hello World")
+
+const print = (el ='Ok') => console.log(el)
+// document.getElementById('title').innerHTML = 'Hello Bangladesh'
+
+
+// var b = 'Shahin';
+
+// const c = 100;
+
+
+// console.log(typeof b)
+
+// This a comment
+
+/*
+Multiline comment
+*/
+
+// let a = 15;
+// if (false){
+//     // Works 
+//     console.log("Inside If")
+// }else if(a%2 == 0){
+//     console.log("Inside Else If")
+// }else{
+//     console.log("Inside else")
+// }
+
+// console.log(10/2)
+
+// function sayHello(name="Tanzim"){
+//     console.log(`Hello ${name}`)
+//     // console.log("Hello "  + name)
+// }
+// sayHello()
+
+// const sayHello = (name = 'Asik') => {
+//     return console.log(`Hello ${name}`)
+// }
+
+// const sayHello = (name = 'Asik') => `Hello ${name}`
+// abc =  sayHello()
+
+// console.log(abc)
+
+
+// let a = 10
+
+// console.log(a)
+// if(true){
+//     let  a = 20
+// }
+
+// console.log(a)
+
+// let pi = 3.1416
+// pi = 555
+
+// console.log(2**3)
+
+const nums = [10,20,30,40];
+
+console.log(typeof nums)
+
+let obj = {
+    'name' : 'Shahin',
+    'age' : 21,
+    'skills' : ['PHP','JS'],
+    'study' : [
+        {
+            'school' :'Jagatpur'
+        },
+        {
+            'college' :'HMC'
+        },
+    ]
+
+}
+
+let person = {
+    'name' : 'ABC',
+    'eat' : function(){
+        return "I am eating"
+    }
+}
+
+// for(var i =0;i<10;i++){
+//     print(i)
+// }
+
+let i = 1;
+
+// while(i <= 15){
+//     print(i);
+//     // if (i==10){
+//     //     break
+//     // }
+
+//     if(i == 5){
+//         continue
+//     }
+//     i++;
+// }
